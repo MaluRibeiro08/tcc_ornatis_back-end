6ae77ec66dbf6e785f85cc743325ebf6766536a2
unknown <maluribeiro08@gmail.com>
2022-04-02 22:54:15 -0300

    Criando rota para seleção de dados gerais da conta

--- api-ornatis/controller/ControllerContaAdministradora.php
+++ api-ornatis/controller/ControllerContaAdministradora.php
@@ -6,6 +6,8 @@ class ControllerContaAdministradora{
     private $_model_admin;
     private $_model_empresa;
     private $_id_administrador;
+    private $_id_empresa;
+    private $_flag;
 
     public function __construct($model_administrador, $model_empresa)
     {
@@ -14,7 +16,16 @@ class ControllerContaAdministradora{
         $this->_model_empresa = $model_empresa;
         $this->_method = $_SERVER['REQUEST_METHOD'];
 
-        $this->_id_administrador = $_POST["id_administrador"] ?? null; 
+        //PEGANDO DADOS DA REQ
+        $json = file_get_contents("php://input");
+        $dados_requisiscao = json_decode($json);
+        $this->_flag = $dados_requisiscao->flag ?? $_POST["flag"] ?? null;;
+
+        //ID_EMPRESA
+        $this->_id_empresa = $dados_requisiscao->id_empresa ?? $_POST["id_empresa"] ?? null;
+
+        //ID_ADM
+        $this->_id_administrador = $dados_requisiscao->id_administrador ?? $_POST["id_administrador"] ?? null; 
 
     }
 
@@ -22,8 +33,30 @@ class ControllerContaAdministradora{
 
         switch ($this->_method) {
             case 'GET':
+                if($this->_id_empresa != null)
+                {
+                    //buscando os dados do adm
+                    $dados_administrador["dados_administrador"]= $this->_model_admin->findByEmpresa();
+
+                    //buscando os dados da empresa
+                    //$dados_empresa["dados_empresa"] = $this->_model_empresa->getInformacoesContaEmpresa();
+                    $dados_empresa["dados_empresa"] = $this->_model_empresa->getInformacoesEmpresa();
+                    $dados_empresa["dados_endereco_empresa"] = $this->_model_empresa->getEnderecoEmpresa();
+                    $dados_empresa["dados_funcionamento"] = $this->_model_empresa->getFuncionamento();
+                    $dados_empresa["dados_pagamento"] = $this->_model_empresa->getFormasPagamento();
+                    $dados_empresa["taxa_cancelamento_empresa"] = $this->_model_empresa->getTaxasCancelamento();
+
+                    //dados de login
+                    $_id_administrador = $dados_administrador["dados_administrador"][0]["id_administrador"];
+                    $dados_login["dados_login"] = $this->_model_admin->getLogin(1);
+
+                    return (array_merge($dados_administrador,$dados_empresa, $dados_login));
+                }
+                else
+                {
+                    return "Deu erro no router da controllerContaAdm";
+                };
                 
-                return $this->_model_admin->findById();
                 break;
 
             case 'POST':
