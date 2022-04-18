1bebf370c3214983d38c0db43aa6e8210f799f24
Welington Pincer <79910007+luuh-oliveira@users.noreply.github.com>
2022-04-18 08:20:32 -0300

    Alterações no recebimento de dados - ControllerContaAdm

--- api-ornatis/controller/ControllerContaAdministradora.php
+++ api-ornatis/controller/ControllerContaAdministradora.php
@@ -12,6 +12,7 @@ class ControllerContaAdministradora
     private $_array_forma_pagamento;
     private $_array_taxas_cancelamento;
     private $_array_imagens_espaco_salao;
+    private $dados_requisicao;
     private $_flag;
 
     public function __construct($model_administrador, $model_empresa)
@@ -23,15 +24,15 @@ class ControllerContaAdministradora
 
         //PEGANDO DADOS DA REQ
         $json = file_get_contents("php://input");
-        $dados_requisiscao = json_decode($json);
+        $dados_requisicao = json_decode($json);
 
-        $this->_flag =  $_GET["acao"] ?? $dados_requisiscao->acao ?? $_POST["acao"] ?? null;;
+        $this->_flag =  $_GET["acao"] ?? $dados_requisicao->acao ?? $_POST["acao"] ?? null;;
 
         //ID_EMPRESA
-        $this->_id_empresa =  $_GET["id_empresa"] ?? $dados_requisiscao->id_empresa ?? $_POST["id_empresa"] ?? null;
+        $this->_id_empresa =  $_GET["id_empresa"] ?? $dados_requisicao->id_empresa ?? $_POST["id_empresa"] ?? null;
 
         //ID_ADM
-        $this->_id_administrador = $_GET["id_administrador"] ?? $dados_requisiscao->id_administrador ?? $_POST["id_administrador"] ?? null;
+        $this->_id_administrador = $_GET["id_administrador"] ?? $dados_requisicao->id_administrador ?? $_POST["id_administrador"] ?? null;
     }
 
     function router()
@@ -83,6 +84,10 @@ class ControllerContaAdministradora
 
             case 'POST':
 
+                //PEGANDO DADOS DA REQ
+                $json = file_get_contents("php://input");
+                $dados_requisicao = json_decode($json);
+
                 if ($this->_flag == "create") {
 
                     $dados_empresa["dados_empresa"] = $this->_model_empresa->createEmpresa();
@@ -90,46 +95,44 @@ class ControllerContaAdministradora
 
                     $dados_empresa["dados_endereco_empresa"] = $this->_model_empresa->createEnderecoEmpresa($idEmpresaCriada);
 
-                    $this->_array_funcionamento = $_POST["dados_funcionamento"];
+                    $this->_array_funcionamento = $dados_requisicao->dados_funcionamento;
                     // var_dump($this->_array_funcionamento);
                     $dados_empresa["dados_funcionamento"] = $this->_model_empresa->createFuncionamento($this->_array_funcionamento, $idEmpresaCriada);
 
-                    $this->_array_forma_pagamento = $_POST["dados_formas_pagamento"];
+                    $this->_array_forma_pagamento = $dados_requisicao->dados_formas_pagamento;
                     $dados_empresa["dados_pagamento"] = $this->_model_empresa->createFormasPagamento($this->_array_forma_pagamento, $idEmpresaCriada);
                     // var_dump($this->_array_forma_pagamento);
 
                     if ($dados_empresa["dados_empresa"]["taxa_unica_cancelamento"] == null) {
 
-                        $this->_array_taxas_cancelamento = $_POST["dados_taxa_cancelamento"];
+                        $this->_array_taxas_cancelamento = $dados_requisicao->dados_taxa_cancelamento;
                         $dados_empresa["dados_taxas_cancelamento"] = $this->_model_empresa->createTaxasCancelamento($this->_array_taxas_cancelamento, $idEmpresaCriada);
                     }
 
-                    $this->_array_imagens_espaco_salao = $_POST["dados_imagens_estabelecimento"];
+                    $this->_array_imagens_espaco_salao = $dados_requisicao->dados_imagens_estabelecimento;
                     $dados_empresa["dados_imagens_estabelecimento"] = $this->_model_empresa->createImagensEstabelecimento($this->_array_imagens_espaco_salao, $idEmpresaCriada);
 
                     $dados_administrador["dados_administrador"] = $this->_model_admin->createAdministrador($idEmpresaCriada);
 
                     return array_merge($dados_empresa, $dados_administrador);
-
                 } elseif ($this->_flag == "updateContaAdministradora") {
 
                     $dados_empresa["dados_empresa"] = $this->_model_empresa->updateEmpresa($this->_id_empresa);
                     $dados_empresa["dados_endereco_empresa"] = $this->_model_empresa->updateEnderecoEmpresa($this->_id_empresa);
 
                     $this->_model_empresa->deleteFuncionamento($this->_id_empresa);
-                    $this->_array_funcionamento = $_POST["dados_funcionamento"];
+                    $this->_array_funcionamento = $dados_requisicao->dados_funcionamento;
                     $dados_empresa["dados_funcionamento"] = $this->_model_empresa->createFuncionamento($this->_array_funcionamento, $this->_id_empresa);
 
                     $this->_model_empresa->deleteFormasPagamento($this->_id_empresa);
-                    $this->_array_forma_pagamento = $_POST["dados_formas_pagamento"];
+                    $this->_array_forma_pagamento = $dados_requisicao->dados_formas_pagamento;
                     $dados_empresa["dados_pagamento"] = $this->_model_empresa->createFormasPagamento($this->_array_forma_pagamento, $this->_id_empresa);
 
                     $this->_model_empresa->deleteTaxasCancelamento($this->_id_empresa);
                     if ($dados_empresa["dados_empresa"]["taxa_unica_cancelamento"] == null) {
 
-                        $this->_array_taxas_cancelamento = $_POST["dados_taxa_cancelamento"];
+                        $this->_array_taxas_cancelamento = $dados_requisicao->dados_taxa_cancelamento;
                         $dados_empresa["dados_taxas_cancelamento"] = $this->_model_empresa->createTaxasCancelamento($this->_array_taxas_cancelamento, $this->_id_empresa);
-                    
                     }
 
                     $dados_administrador[] = $this->_model_admin->findIdByEmpresa();
@@ -138,13 +141,11 @@ class ControllerContaAdministradora
                     $dados_administrador["dados_administrador"] = $this->_model_admin->updateAdministrador($this->_id_empresa, $this->_id_administrador);
 
                     return array_merge($dados_empresa, $dados_administrador);
-
                 } elseif ($this->_flag == "updateRedesSociais") {
-                    
+
                     $redesSociais = $this->_model_empresa->updateRedesSociais();
                     return $redesSociais;
-
-                } 
+                }
 
 
             default:
